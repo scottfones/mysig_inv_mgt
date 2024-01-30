866176d9adea94a306725b1c182fe9b9745b0f62
Scott Fones <scott.fones@gmail.com>
2024-01-30 15:43:59 -0500

    added accent color support. all accumulators are now counters and report category/total counts. fixed line item print middle newline

--- orig_csv/csv_read.py
+++ orig_csv/csv_read.py
@@ -55,9 +55,10 @@ def plot_widths(widths_x: list[float], widths_y: list[int]):
 if __name__ == "__main__":
     csv_path = Path("./Mysig Cookie Cutters - Sheet1.csv")
 
-    bin_names = set()
-    materials = set()
-    colors = set()
+    bin_names = []
+    materials = []
+    accents = []
+    colors = []
     heights_list = []
     widths_list = []
     seq_count = 0
@@ -71,7 +72,9 @@ if __name__ == "__main__":
             # Bin Name
             bin_name = " ".join(row[0].splitlines()).replace("  ", " ")
             if bin_name:
-                bin_names.add(bin_name)
+                bin_names.append(bin_name)
+            else:
+                bin_names.append("NONE")
 
             # Bin and Sequence Count
             if row[1]:
@@ -87,12 +90,22 @@ if __name__ == "__main__":
 
             # Material
             if row[3]:
-                materials.add(row[3])
+                materials.append(row[3])
+            else:
+                materials.append("NONE")
 
             # Color
+            accent = "NONE"
             color = " ".join(row[4].splitlines()).replace("  ", " ")
-            if color:
-                colors.add(color)
+            if color and "W/" in color:
+                color, accent = color.split(" W/ ")
+                accent_parts = accent.split()
+                if len(accent_parts) == 4:
+                    accent = " ".join(accent.split()[0:2])
+                else:
+                    accent = accent_parts[0]
+            accents.append(accent)
+            colors.append(color)
 
             # Height and Width
             height, width = (
@@ -113,30 +126,39 @@ if __name__ == "__main__":
             print(f"Quantity: {quantity}")
             print(f"Keywords: {desc}, {notes}")
             print(f"Height: {height}")
-            print(f"Width: {width}\n")
+            print(f"Width: {width}")
             print(f"Material: {row[3]}")
             print(f"Color: {color}")
+            print(f"Accent: {accent}\n")
 
             # Picture
 
             # Notes
 
-    print(f"bin names ({len(bin_names)}):")
-    for name in sorted(list(bin_names)):
-        print(f"\t{name}")
+    bin_counter = Counter(bin_names)
+    print(f"\ncolors ({len(bin_counter)}, {sum(bin_counter.values())}):")
+    for bin, count in sorted(bin_counter.items()):
+        print(f"\t{bin}  ({count})")
 
-    print(f"\nmaterials ({len(materials)}):")
-    for mat in sorted(list(materials)):
-        print(f"\t{mat}")
+    materials_counter = Counter(materials)
+    print(f"\ncolors ({len(materials_counter)}, {sum(materials_counter.values())}):")
+    for mat, count in sorted(materials_counter.items()):
+        print(f"\t{mat}  ({count})")
 
-    print(f"\ncolors ({len(colors)}):")
-    for color in sorted(list(colors)):
-        print(f"\t{color}")
+    colors_counter = Counter(colors)
+    print(f"\ncolors ({len(colors_counter)}, {sum(colors_counter.values())}):")
+    for color, count in sorted(colors_counter.items()):
+        print(f"\t{color}  ({count})")
+
+    accents_counter = Counter(accents)
+    print(f"\naccents ({len(accents_counter)}, {sum(accents_counter.values())}):")
+    for accent, count in sorted(accents_counter.items()):
+        print(f"\t{accent}  ({count})")
 
     heights_x = []
     heights_y = []
     heights_counter = Counter(heights_list)
-    print(f"\nheights ({len(heights_counter)}):")
+    print(f"\nheights ({len(heights_counter)}, {sum(heights_counter.values())}):")
     for height, count in sorted(heights_counter.items()):
         heights_x.append(height)
         heights_y.append(count)
@@ -145,7 +167,7 @@ if __name__ == "__main__":
     widths_x = []
     widths_y = []
     widths_counter = Counter(widths_list)
-    print(f"\nwidths ({len(widths_counter)}):")
+    print(f"\nwidths ({len(widths_counter)}, {sum(widths_counter.values())}):")
     for width, count in sorted(widths_counter.items()):
         widths_x.append(width)
         widths_y.append(count)
